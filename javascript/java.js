610d5c277f66dd23158d92c1f5373d053bba9ab7
Admin <admin@Iambeejay-MacBook-Pro.local>
2022-12-15 22:22:28 -0700

    ggg

---
+++ javascript/java.js
@@ -0,0 +1,9 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function() {
+  fields.firstName = document.getElementById('firstName');
+  fields.lastName = document.getElementById('lastName');
+  fields.email = document.getElementById('email');
+  fields.address = document.getElementById('address');
+  fields.houseNumber = document.getElementById('age');
+ })
