2afa0780de1daa861eadb4e278c68caf506c234d
Rogue-01 <113165538+Rogue-01@users.noreply.github.com>
2022-12-16 18:04:07 -0700

    update

--- javascript/java.js
+++ javascript/java.js
@@ -1,16 +1,13 @@
-function showInput() {
-  var nameInput = document.getElementById("name").value;
-  var ageInput = document.getElementById("age").value;
-  var emailInput = document.getElementById("e-mail").value;
-  var addressInput = document.getElementById("address").value;
-  var messageInput = document.getElementById("message").value;
-  document.getElementById("display").innerHTML = nameInput;
-  document.getElementById("display").innerHTML = ageInput;
-  document.getElementById("display").innerHTML = emailInput;
-  document.getElementById("display").innerHTML = addressInput;
-  document.getElementById("display").innerHTML = messageInput;
-}
- 
+
+
+$(document).ready(function() {
+  $("button").click(function() {
+         
+      var nameInput = $("input:text").val();
+      document.getElementById("display").innerHTML = nameInput;
+      
+    });
+  });
  
  $( '.js-input' ).keyup(function() {
   if( $(this).val() ) {
